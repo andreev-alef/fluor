1a502f33341e22a9afaca6fe2530b7faa20daf95
netyaga <netyaga@ivcgzo.nkz.ru>
2014-12-03 16:17:55 +0700

    Создание всех экранных форм

---
+++ src/main/java/ru/miacn/persistence/RefDecrGroup.java
@@ -0,0 +1,29 @@
+package ru.miacn.persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "r_decr_group")
+public class RefDecrGroup {
+	@Id
+	private String id;
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
